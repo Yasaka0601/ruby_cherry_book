7659c39e25f0624bcd5617930cf6b1291606a6ff
Yasaka0601 <www.gtr.0601@icloud.com>
2026-05-08 15:42:50 +0900

    May.8

--- lib/convert_hash_syntax.rb
+++ lib/convert_hash_syntax.rb
@@ -1,3 +1,17 @@
+# old_syntax = <<~TEXT
+#   {
+#     :name   => 'Alice',
+#     :age    => 20,
+#     :gender => :female
+#   }
+# TEXT
+
 def convert_hash_syntax(old_syntax)
-  old_syntax
+  old_syntax.gsub(/:(\w+) *=> */)do
+    "#{$1}: "
+  end
 end
+
+# actual = convert_hash_syntax(old_syntax)
+
+# puts actual
